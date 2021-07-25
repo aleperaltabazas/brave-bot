ba4852388feff4102e71f2ed8c7d3ede8a172afe
Alejandro <aleperaltabazas@hotmail.com>
2021-07-25 14:01:01 -0300

    download image and attach it (I should consider caching the images), better tagging as well

--- src/main/kotlin/com/github/aleperaltabazas/brave/BraveBot.kt
+++ src/main/kotlin/com/github/aleperaltabazas/brave/BraveBot.kt
@@ -5,12 +5,16 @@ import com.fasterxml.jackson.dataformat.csv.CsvSchema
 import com.github.aleperaltabazas.brave.model.Unit
 import com.typesafe.config.ConfigFactory
 import dev.kord.core.Kord
+import dev.kord.core.behavior.channel.createMessage
 import dev.kord.core.event.message.MessageCreateEvent
 import dev.kord.core.on
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import org.slf4j.LoggerFactory
 import spark.Spark
+import java.net.URL
 
 private val SCHEMA = CsvSchema.emptySchema()
     .withHeader()
@@ -24,7 +28,7 @@ fun main() = runBlocking {
     LOGGER.info("Using port $port")
 
     Spark.port(port)
-    Spark.get("/*") { req, res -> "I feel fantastic and I'm still alive" }
+    Spark.get("/*") { _, _ -> "I feel fantastic and I'm still alive" }
 
     val config = ConfigFactory.load()
     val client = Kord(config.getString("discord.bot.token"))
@@ -52,12 +56,16 @@ fun main() = runBlocking {
                     ?: units.random()
 
             val url = unit.arts.random()
-            val m = listOfNotNull(
-                "You summoned **${unit.name}**!",
-                url,
-            )
-            val response = message.channel.createMessage(m.joinToString("\n"))
-            response.attachments.forEach { it.isImage }
+
+            launch(Dispatchers.IO) {
+                val input = URL(url).openStream()
+                val fileName = url.reversed().drop(1).takeWhile { it != '/' }.reversed()
+
+                message.channel.createMessage {
+                    this.content = "<@${message.author?.id?.asString}> summoned **${unit.name}**!"
+                    this.addFile(fileName, input)
+                }
+            }
         } catch (e: Exception) {
             LOGGER.error("An error occurred", e)
             message.channel.createMessage("Beep boop andá a mirar a los logs")
